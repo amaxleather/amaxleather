05371259fe2d5a62da684ae3dce60190ccd69c0d
amaxleather <52262165+amaxleather@users.noreply.github.com>
2019-07-14 02:19:58 -0400

    Update leathercare.js

--- src/data/leathercare.js
+++ src/data/leathercare.js
@@ -20,6 +20,13 @@ export default () => {
       <h3>Room Temperature</h3>
       <p>Position your leather furniture away from direct sunlight, high-wattage lights, heating and air conditioning
         units to prevent leather from drying out.</p>
+      <h3>Caring Tips</h3>
+      <p>It is normal for seat cushions to become ‘worn in’ and soften overtime. In order to ensure that cushions return
+        to their intended look, you can simply massage or smooth the cushions by hand. Do not jump on the sofa, or sit
+        on the arm or back.</p>
+      <h3>Furniture Handling</h3>
+      <p>If the sofa needs to be moved, avoid lifting from the armrest and arm pillows; the correct way to move the sofa
+        is by lifting from the base or underside of the arms. Never move the sofa by pushing or pulling it.</p>
     </div>
   );
 };
